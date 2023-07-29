bdb432534e427f7002e9b953e7bed8470978fd5a
sidul567 <sidulsam@gmail.com>
2023-07-30 01:16:10 +0600

    First Commit

--- server/utilities/sendEmail.js
+++ server/utilities/sendEmail.js
@@ -11,19 +11,6 @@ const sendEmail = async (options)=>{
         port: process.env.SMTP_PORT
     })
 
-    await new Promise((resolve, reject) => {
-        // verify connection configuration
-        transporter.verify(function (error, success) {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log("Server is ready to take our messages");
-                resolve(success);
-            }
-        });
-    });
-
     const mailOptions = {
         from: process.env.SMTP_EMAIL,
         to: options.email,
