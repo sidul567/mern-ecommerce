dd4e35a8cac2e19db0cebdf0d103a84baf190213
sidul567 <sidulsam@gmail.com>
2023-08-01 01:49:51 +0600

    First Commit

--- client/src/host.js
+++ client/src/host.js
@@ -1,2 +1,3 @@
 // export const HOST = "http://localhost:5000";
-export const HOST = "https://mern-ecommerce-ashen.vercel.app";
+// export const HOST = "https://mern-ecommerce-ashen.vercel.app";
+export const HOST = "https://mern-ecommerce-b7k6.onrender.com";
--- server/app.js
+++ server/app.js
@@ -28,9 +28,9 @@ app.use(cors({
         'Authorization' 
     ]
 }));
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({'extended': true}));
+app.use(bodyParser.urlencoded({limit:'50mb', 'extended': true}));
 
 // router import
 const productRoute = require('./routes/productRoute');
